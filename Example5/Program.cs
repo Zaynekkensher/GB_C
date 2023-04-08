9d419776dc8f6ea095f21e957e01f733019b4b08
Zaynekkensher <kiyashko.v@me.com>
2023-04-08 13:38:47 +0300

    safd

---
+++ Example5/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "хуйло")
+{
+    Console.WriteLine("ЗДАРОВО ХУЙЛО!!!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+    Console.Write("!!!");
+    }
