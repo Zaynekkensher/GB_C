b6e871d49baaf729aa12537daf637107eba46548
Zaynekkensher <kiyashko.v@me.com>
2023-04-08 23:23:43 +0300

    1234

---
+++ Example10/Program.cs
@@ -0,0 +1,12 @@
+﻿ Console.Write("Enter number : ");
+
+ int number = Convert.ToInt32(Console.ReadLine());
+
+ int number2 = - number;
+ 
+ while(number2 <= number)
+ {
+ Console.Write($"{number2},");
+ 
+ ++number2;
+ }
