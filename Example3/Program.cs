0f520e11232c2012f64269250bb6957c9698190e
Zaynekkensher <kiyashko.v@me.com>
2023-04-08 12:58:38 +0300

    edfsaw

--- Example3/Program.cs
+++ Example3/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Введите ваше имя      ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write(username);
+﻿int a = 31;
+int b = 31;
+int c = a + b;
+Console.WriteLine(c);
